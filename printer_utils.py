d30f3a0e404576cb1d578141c4bbdb04c4a7e813
Andrew Moscalev <admin@asimple.ru>
2019-12-11 18:08:28 +0300

    tempfile instead of crunch

--- printer_utils.py
+++ printer_utils.py
@@ -4,9 +4,12 @@ from PIL import Image
 from PyPDF2 import PdfFileReader
 from pathlib import Path
 import img2pdf
+import tempfile
+
 
 class PrinterFileException(Exception):
     pass
+
 
 class PrinterFile:
     pdf = None
@@ -40,24 +43,25 @@ class PrinterFile:
             png.load()
             background = Image.new("RGB", png.size, (255, 255, 255))
             background.paste(png, mask=png.split()[3])  # 3 is the alpha channel
-            jpg_path = self.file.parent / 'tempfile.jpg'
-            pdf_path = self.file.parent / 'tempfile.pdf'
+            wd = tempfile.TemporaryDirectory()
+            work_dir = Path(wd.name)
+            jpg_path = work_dir / 'tempfile.jpg'
+            pdf_path = work_dir / 'tempfile.pdf'
             background.save(jpg_path, 'JPEG', quality=80)
             with open(pdf_path, "wb") as f:
                 f.write(img2pdf.convert(f'{jpg_path}', layout_fun=layout_fun))
-            os.remove(jpg_path)
             self.load_pdf(pdf_path)
-            os.remove(pdf_path)
         elif self.extension == 'jpg':
             self.file_type = 'jpg'
-            pdf_path = self.file.parent / 'tempfile.pdf'
+            wd = tempfile.TemporaryDirectory()
+            work_dir = Path(wd.name)
+            pdf_path = work_dir / 'tempfile.pdf'
             # specify paper size (A4)
             a4inpt = (img2pdf.mm_to_pt(210), img2pdf.mm_to_pt(297))
             layout_fun = img2pdf.get_layout_fun(a4inpt)
             with open(pdf_path, "wb") as f:
                 f.write(img2pdf.convert(f'{self.path}', layout_fun=layout_fun))
             self.load_pdf(pdf_path)
-            os.remove(pdf_path)
 
     def get_pages_count(self):
         if self.file_type in ['pdf', 'png']:
@@ -73,7 +77,6 @@ class PrinterFile:
 if __name__ == '__main__':
     # file = PrinterFile("/Users/andrewmoskalev/Downloads/Копия Go plakat.pdf")
     file = PrinterFile("/Users/andrewmoskalev/PycharmProjects/PrinterCore/Снимок экрана 2019-12-10 в 23.36.57.png")
-
     print(f'Информация о "{file.name}"')
     print(f'Тип файла: {file.file_type}')
     print(f'Страниц: {file.get_pages_count()}')
